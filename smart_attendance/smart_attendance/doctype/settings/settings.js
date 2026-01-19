5f0d9f002ed9b56d2481a2df9a9b2582be540237
rutuja <rsawant@dexciss.io>
2026-01-19 23:20:50 +0530

    add doctype

---
+++ smart_attendance/smart_attendance/doctype/settings/settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, pradip and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ smart_attendance/smart_attendance/doctype/settings/settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, pradip and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Settings(Document):
+	pass
